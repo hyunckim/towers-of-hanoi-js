99ee8442af71c1cc974671ad33c18461e8afe818
App Academy Student <student@appacademy.io>
2017-02-23 15:39:51 -0800

    Create our own #bind function

--- callbacks_exercises.js
+++ callbacks_exercises.js
@@ -102,8 +102,34 @@ function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
     outerBubbleSortLoop(madeAnySwaps);
   }
 }
+reader.close();
+// absurdBubbleSort([3, 2, 1], function (arr) {
+//   console.log("Sorted array: " + JSON.stringify(arr));
+//   reader.close();
+// });
 
-absurdBubbleSort([3, 2, 1], function (arr) {
-  console.log("Sorted array: " + JSON.stringify(arr));
-  reader.close();
-});
+Function.prototype.myBind = function(context) {
+  return () => {
+    this.apply(context);
+  };
+};
+
+class Lamp {
+  constructor() {
+    this.name = "a lamp";
+  }
+}
+
+const turnOn = function() {
+   console.log("Turning on " + this.name);
+};
+
+const lamp = new Lamp();
+
+// turnOn(); // should not work the way we want it to
+
+const boundTurnOn = turnOn.bind(lamp);
+const myBoundTurnOn = turnOn.myBind(lamp);
+
+// boundTurnOn(); // should say "Turning on a lamp"
+// myBoundTurnOn(); // should say "Turning on a lamp"
