4098abffafaaf112edff1c55d80dcfb45f60b050
App Academy Student <student@appacademy.io>
2017-02-23 15:30:59 -0800

    Create #absurdBubbleSort function

--- callbacks_exercises.js
+++ callbacks_exercises.js
@@ -69,4 +69,41 @@ function addNumbers(sum, numsLeft, completionCallback) {
   }
 }
 
-addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+
+function absurdBubbleSort(arr, sortCompletionCallback) {
+  function outerBubbleSortLoop(madeAnySwaps) {
+    if (madeAnySwaps) {
+      innerBubbleSortLoop(arr, 0, false, outerBubbleSortLoop);
+    } else { sortCompletionCallback(arr); }
+  }
+  outerBubbleSortLoop(true);
+}
+
+function askIfGreaterThan(el1, el2, callback) {
+  reader.question(`Is ${el1} greater than ${el2}? `, response => {
+    if (response === 'yes') { callback(true); }
+    else { callback(false); }
+  });
+}
+
+function innerBubbleSortLoop(arr, i, madeAnySwaps, outerBubbleSortLoop) {
+  if (i < arr.length - 1) {
+    askIfGreaterThan(arr[i], arr[i + 1], isGreaterThan => {
+      if (isGreaterThan) {
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        madeAnySwaps = true;
+      }
+      innerBubbleSortLoop(arr, i + 1, madeAnySwaps, outerBubbleSortLoop);
+    });
+  }
+  if (i === (arr.length - 1)) {
+    outerBubbleSortLoop(madeAnySwaps);
+  }
+}
+
+absurdBubbleSort([3, 2, 1], function (arr) {
+  console.log("Sorted array: " + JSON.stringify(arr));
+  reader.close();
+});
